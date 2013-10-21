3731e5921736081081aff6154245cd75b7c5dca4
xkings <xkingsgames@gmail.com>
2013-10-21 21:29:03 +0200

    Adding inheritance support for mapper anotations, during mapper initialization whole class hierarchy is searched.

--- src/com/artemis/annotations/Mapper.java
+++ src/com/artemis/annotations/Mapper.java
@@ -1,10 +1,8 @@
 package com.artemis.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Mapper {
